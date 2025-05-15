2ad938b0e79a45803c485beb9732fa37d1ef8dd3
vinod-rajasekaran <flans-boules0n@icloud.com>
2025-05-15 08:38:57 +0530

    added anc outcomes data

---
+++ models/production/mch/birth_outcomes.sql
@@ -0,0 +1,8 @@
+{{ config(
+  tags=['anc'],
+  schema='production'
+) }}
+
+select *,
+       case when weight_at_birth > 2.5 then 'Yes' else 'No' end as healthy_birth_weight
+ from {{ ref('int_birth_outcomes') }} 
