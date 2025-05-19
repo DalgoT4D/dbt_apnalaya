8c89d3a207a9d6728d26be81801595e34a77c7b0
vinod-rajasekaran <flans-boules0n@icloud.com>
2025-05-19 15:34:51 +0530

    Update int_birth_outcomes.sql

--- models/intermediate/mch/int_birth_outcomes.sql
+++ models/intermediate/mch/int_birth_outcomes.sql
@@ -3,18 +3,19 @@
     schema='intermediate'
 ) }}
 
-SELECT 
-    'AIMS' as source,
-    child_unique_id as child_id,
-    "Ward" as ward,
-    weight_at_birth_kg_gm::float as weight_at_birth,
-    (NULLIF("DOB",'NA'))::date as date_of_birth
-FROM {{ source('anc', 'aims_birth_outcomes') }}
-UNION ALL
+-- Exclude AIMS data due to integer birth weight 
+-- SELECT 
+--     'AIMS' as source,
+--     child_unique_id as child_id,
+--     "Ward" as ward,
+--     weight_at_birth_kg_gm::float as weight_at_birth,
+--     (NULLIF("DOB",'NA'))::date as date_of_birth
+-- FROM {{ source('anc', 'aims_birth_outcomes') }}
+-- UNION ALL
 (SELECT 
     'Aanandimma' as source,
     concat(mother_id,edd_date) as child_id,
     ward,
     outcome_children_weight::float as weight_at_birth,
     outcome_date::date as date_of_birth
-FROM {{ source('anc', 'aanandimaa_anc') }} where outcome_children_weight != 'NA')
+FROM {{ source('anc', 'aanandimaa_anc') }} where outcome_children_weight != 'NA' or birthType != 'Still Birth')
