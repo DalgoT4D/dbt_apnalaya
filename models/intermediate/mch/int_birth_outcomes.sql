f9ea71de7aa545dc6d118c0e5608dfa497b961e1
vinod-rajasekaran <flans-boules0n@icloud.com>
2025-05-19 20:35:08 +0530

    removed aims and fixed zero birthweight cases

--- models/intermediate/mch/int_birth_outcomes.sql
+++ models/intermediate/mch/int_birth_outcomes.sql
@@ -3,6 +3,17 @@
     schema='intermediate'
 ) }}
 
+with cte_anc_cases as (
+    SELECT 
+    'Aanandimma' as source,
+    concat(mother_id,edd_date) as child_id,
+    ward,
+    outcome_children_weight::float as weight_at_birth,
+    outcome_date::date as date_of_birth,
+    "birthType" as birth_type
+FROM {{ source('anc', 'aanandimaa_anc') }} where outcome_children_weight != 'NA' 
+)
+
 -- Exclude AIMS data due to integer birth weight 
 -- SELECT 
 --     'AIMS' as source,
@@ -12,10 +23,4 @@
 --     (NULLIF("DOB",'NA'))::date as date_of_birth
 -- FROM {{ source('anc', 'aims_birth_outcomes') }}
 -- UNION ALL
-(SELECT 
-    'Aanandimma' as source,
-    concat(mother_id,edd_date) as child_id,
-    ward,
-    outcome_children_weight::float as weight_at_birth,
-    outcome_date::date as date_of_birth
-FROM {{ source('anc', 'aanandimaa_anc') }} where outcome_children_weight != 'NA' or birthType != 'Still Birth')
+SELECT * FROM cte_anc_cases where birth_type != 'Still Birth'
