49d5a77234f638e5761c956ca1a3ef2cee502f2e
vinod-rajasekaran <flans-boules0n@icloud.com>
2025-06-11 13:10:08 +0530

    trimester registration metric

--- models/intermediate/mch/int_anc.sql
+++ models/intermediate/mch/int_anc.sql
@@ -27,7 +27,8 @@ with
         CASE WHEN b.place_of_delivery = '#NA' THEN NULL
                  WHEN b.place_of_delivery IN ('Private Institution', 'Private Hospital') THEN 'Private'
                  ELSE 'Government' END as place_of_delivery,
-        (COALESCE(b.date_deli_termination_pregnancy,a.expected_date_of_delivery))::date as date_of_delivery
+        (COALESCE(b.date_deli_termination_pregnancy,a.expected_date_of_delivery))::date as date_of_delivery,
+        case when a.reg_date_health_facility::date - date_of_last_menstrual_period::date < 91 then 'Yes' else 'No' end as registration_in_first_trimester
     from cte_aims_valid_cases as a
     LEFT JOIN (select preg_registration_unique_id, place_of_delivery, date_deli_termination_pregnancy from {{ source('anc', 'aims_pnc') }}) as b 
     ON a.preg_registration_unique_id=b.preg_registration_unique_id),
@@ -50,7 +51,8 @@ with
             CASE WHEN "outcomePlaceOfDelivery" = '#NA' THEN NULL
                  WHEN "outcomePlaceOfDelivery" IN ('Private Institution', 'Private Hospital') THEN 'Private'
                  ELSE 'Government' END as place_of_delivery,
-            (COALESCE(outcome_date,edd_date))::date as date_of_delivery
+            (COALESCE(outcome_date,edd_date))::date as date_of_delivery,
+            case when (NULLIF("govtOrPrivRegistrationDate",'NA'))::date - (NULLIF("lmpDate",'NA'))::date < 91 then 'Yes' else 'No' end as registration_in_first_trimester
         from {{ source('anc', 'aanandimaa_anc') }} where "outcomeDeliveryStatus" = 'Delivered')
 
 Select * from cte_aims_anc
